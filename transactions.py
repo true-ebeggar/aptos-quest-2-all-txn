a7b7415c2e8224848aa36aff2276c38d760be64a
Reilighost <2077netrunner@gmail.com>
2023-12-26 12:47:57 -1100

    add open_merkle_order()
    Txn is fake, despite successful execution no actual position will be opened, txn still visible in explorer btw.

--- transactions.py
+++ transactions.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import random
 
 from aptos_sdk.account import Account
 from aptos_sdk.client import RestClient
@@ -14,12 +15,15 @@ Z6 = 10**6
 Rest_Client = RestClient("https://fullnode.mainnet.aptoslabs.com/v1")
 
 def get_apt_price():
-    token_name = "aptos"
-    url = f"https://api.coingecko.com/api/v3/simple/price?ids={token_name}&vs_currencies=usd"
-    response = requests.get(url)
-    data = response.json()
-    token_price_usd = data[token_name]['usd']
-    return token_price_usd
+    try:
+        response = requests.get('https://app.merkle.trade/api/v1/summary/prices')
+        for pair in response.json()["pairs"]:
+            if pair["id"] == "APT_USD":
+                APT_usd_price = pair["price"]
+                return APT_usd_price
+    except Exception:
+        return None
+
 def submit_and_log_transaction(account, payload, logger):
     try:
         txn = Rest_Client.submit_transaction(account, payload)
@@ -29,8 +33,15 @@ def submit_and_log_transaction(account, payload, logger):
         logger.error(f"AssertionError caught: {e}")
     except Exception as e:
         logger.critical(f"An unexpected error occurred: {e}")
+
+def append_digit_to_integer(original_integer, digit_to_add):
+    new_integer_string = str(original_integer) + str(digit_to_add)
+    new_integer = int(new_integer_string)
+    return new_integer
+
 def get_account_balance(client, account):
     logger = setup_gay_logger('get_account_balance')
+
     try:
         return int(client.account_balance(account_address=account.address()))
     except Exception as e:
@@ -64,10 +75,11 @@ def swap_zUSDC_to_MOD(account, amount_zUSDC: int):
             str(MOD_slip_int)
         ],
     }
+
     submit_and_log_transaction(account, payload, logger)
 
 def check_registration(address, to_check: str):
-    logger = setup_gay_logger('check_zUSDC_registration')
+    logger = setup_gay_logger(f'check_<{to_check}>_registration')
     try:
         coin_type = f"0x1::coin::CoinStore<{to_check}>"
         url = f"https://fullnode.mainnet.aptoslabs.com/v1/accounts/{address}/resources?limit=9999"
@@ -78,7 +90,7 @@ def check_registration(address, to_check: str):
         logger.error(f"An error occurred: {e}")
         return False
 
-def deposit_MOD(account, amount_MOD: int):
+def stake_MOD(account, amount_MOD: int):
     logger = setup_gay_logger('deposit_MOD')
 
     payload = {
@@ -91,6 +103,7 @@ def deposit_MOD(account, amount_MOD: int):
             str(amount_MOD)
         ],
     }
+
     submit_and_log_transaction(account, payload, logger)
 
 def register_coin(account, to_register: str):
@@ -104,10 +117,11 @@ def register_coin(account, to_register: str):
         ],
         "arguments": []
     }
+
     submit_and_log_transaction(account, payload, logger)
 
-def swap_APT_to_zUSDC(account, amount: int):
-    logger = setup_gay_logger('swap_APT_to_zUSDC')
+def swap_APT_to_zUSDC_via_liquidswap(account, amount: int):
+    logger = setup_gay_logger('swap_APT_to_zUSDC_via_liquidswap')
 
     apt_price = get_apt_price()
     normalization = amount / Z8
@@ -128,4 +142,44 @@ def swap_APT_to_zUSDC(account, amount: int):
             str(zUSDC_slip_int)
         ],
     }
+
+    submit_and_log_transaction(account, payload, logger)
+def open_merkle_order(account, amount_zUSDC: int):
+    # Fake tx, no actual position will be open
+
+    logger = setup_gay_logger('open_merkle_order')
+
+    leverage = 130
+    position_size = leverage * amount_zUSDC
+    if position_size <= 300000000:
+        return None
+
+    apt = int(get_apt_price() * Z8)
+    margin_requirement = 1 / leverage
+    liquidation_price = int(apt * (1 - margin_requirement))
+    stop_loss_price = int(apt * (1 - 0.10 / leverage))
+    take_profit_price = int(apt * (1 + 0.20 / leverage))
+
+
+    payload = {
+        "function": "0x5ae6789dd2fec1a9ec9cccfb3acaf12e93d432f0a3a42c92fe1a9d490b7bbc06::managed_trading::place_order_with_referrer",
+        "type_arguments": [
+            "0x5ae6789dd2fec1a9ec9cccfb3acaf12e93d432f0a3a42c92fe1a9d490b7bbc06::pair_types::APT_USD",
+            "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC"
+        ],
+        "arguments": [
+            str(position_size),
+            str(amount_zUSDC),
+            str(append_digit_to_integer(liquidation_price, 10)),
+            True,
+            True,
+            True,
+            str(append_digit_to_integer(stop_loss_price, 59)),
+            str(append_digit_to_integer(take_profit_price, 60)),
+            False,
+            "0x0"
+        ],
+        "type": "entry_function_payload"
+    }
+
     submit_and_log_transaction(account, payload, logger)
