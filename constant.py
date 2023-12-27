d873a220027ac9a5c1a02bffcef760d675affc2b
Reilighost <reilighost@gmail.com>
2023-12-26 14:40:34 -1100

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,5 +1,4 @@
 MAX_SLIPPAGE_PERCENT = 2
 
-ddd= ""
 zUSDC_coin = "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC"
 MOD_coin = "0x6f986d146e4a90b828d8c12c14b6f4e003fdff11a8eecceceb63744363eaac01::mod_coin::MOD"
